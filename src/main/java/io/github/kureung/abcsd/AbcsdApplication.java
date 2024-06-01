004270fbe2fa47f102e2e8bb25dc9b18b478db58
kureung <terry9611@naver.com>
2024-06-02 02:20:52 +0900

    first commit

---
+++ src/main/java/io/github/kureung/abcsd/AbcsdApplication.java
@@ -0,0 +1,13 @@
+package io.github.kureung.abcsd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbcsdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AbcsdApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/kureung/abcsd/AbcsdApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.kureung.abcsd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbcsdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
